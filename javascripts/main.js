a85cb55c06b2b85af6d3892487df700d11d129e7
ellisthomas <ellisthomas80@gmail.com>
2017-05-13 09:41:35 -0500

    saved changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,25 +2,52 @@ $(document).ready(function() {
 
     // let myCharacters = [];
     let printChar = "";
-
+    
 
     const output = $("#container");
 
+    // $("#x-men").on("click", (event) => {
+    //         teamXmen = $("#").val();
+    //         loadWeather(teamXmen).then((results) => {
+    //             console.log("results", results);
+    //             dataGetter(printChar)
+    //         }).catch((error) => {
 
-    $(".btn").click((event) => {
-        $("#background").hide();
-        printChar = ($(event.currentTarget).attr("id"));
-        console.log($(event.currentTarget));
-        // loadChar(team_id).then((results) => {
-        //     // writeToDom(data.characters);
-        // }).catch((error) => {
-        //     console.log(myCharacters);
-        // });
-        dataGetter(printChar);
-    });
+    //         });
+
+    //     });
+xmenButton.click((event)=>{
+    dataGetter.($(event.currentTarget)[0].id);
+    console.log("inside xmen");
+    $("#background").hide();
+});
+
+avengersButton.click((event)=>{
+    dataGetter.($(event.currentTarget)[1].id);
+    console.log("inside avengers");
+    $("#background").hide();
+});
+
+guardiansButton.click((event)=>{
+    dataGetter.($(event.currentTarget)[2].id);
+    console.log("inside guardians");
+    $("#background").hide();
+});
+
+    // $(".btn").click((event) => {
+        // $("#background").hide();
+    //     printChar = ($(event.currentTarget).attr("id"));
+    //     console.log($(event.currentTarget));
+    //     // loadChar(team_id).then((results) => {
+    //     //     // writeToDom(data.characters);
+    //     // }).catch((error) => {
+    //     //     console.log(myCharacters);
+    //     // });
+    //     dataGetter(printChar);
+    // });
 
 
-// add modulous by 4
+    // add modulous by 4
 
 
     const writeToDom = (myCharacters, teamName) => {
@@ -28,7 +55,7 @@ $(document).ready(function() {
         let domString = "";
         for (let i = 0; i < myCharacters.length; i++) {
             domString += `<div class="col-md-3">`;
-			domString += `<p class="name">${myCharacters[i].name}</p>`;
+            domString += `<p class="name">${myCharacters[i].name}</p>`;
             if (myCharacters[i].gender_name === "Male") {
                 domString += `<img class="male" src="${myCharacters[i].image}">`;
             } else if (myCharacters[i].gender_name === "Female") {
@@ -39,8 +66,8 @@ $(document).ready(function() {
             } else if (myCharacters[i].gender_name === "Female" && myCharacters[i].description === "") {
                 domString += `<p class="f-description">1234567890</p>`;
             }
-			domString += `<p>${myCharacters[i].description}</p>`;
-			domString += `</div>`;
+            domString += `<p>${myCharacters[i].description}</p>`;
+            domString += `</div>`;
         }
         $("#container").append(domString);
     };
@@ -83,7 +110,7 @@ $(document).ready(function() {
     // };
 
     const dataGetter = (selectedTeam, printChar) => {
-    Promise.all([loadChar(), loadGender(), loadTeams()])
+        Promise.all([loadChar(), loadGender(), loadTeams()])
             .then((results) => {
                 console.log("results", results);
                 let characters = results[0];
@@ -93,41 +120,41 @@ $(document).ready(function() {
                 characters.forEach((character) => {
                     teams.forEach((team) => {
                         genders.forEach((gender) => {
-                            if(character.team_id === team.id){
+                            if (character.team_id === team.id) {
                                 character.team_name = team.name;
                             }
 
-                            if(character.gender_id === gender.id){
+                            if (character.gender_id === gender.id) {
                                 character.gender_name = gender.type;
                             }
 
-                            // if(character.gender_id === description.id){
-                            //     character.gender_name === gender.type;
-                            // }
+                            if (char.description === '' && char.gender === 'female') {
+                                char.description = "abcde fghij klmno pqrst uvwxy z"
+                            } else if (char.description === '' && char.gender === 'male') {
+                                char.description = '1234567890';
+                            }
 
                             //add if statement for missing descriptions
-                        });    
+                        });
                     });
                 });
                 console.log("Characters", characters);
                 writeToDom(characters, selectedTeam);
-                
+
             });
-    };        
+    };
 
     // const dataGetter = () => {
-	   //  Promise.all([loadChar(), loadGender(), loadTeams()])
-	   //      .then((results) => {
-	   //          teamChecker(results[0], results[2], printChar);
+    //  Promise.all([loadChar(), loadGender(), loadTeams()])
+    //      .then((results) => {
+    //          teamChecker(results[0], results[2], printChar);
     //             // writeToDom(myCharacters);
     //             console.log("myCharacters",myCharacters);
-	   //     })
+    //     })
     //         .catch((error) => {
     //             console.log("error in Promise All", error);
     //         });
     // };
-    
-
 
 
 
